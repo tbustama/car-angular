7804054262f9af889bfb826438a8baf80b4c430a
Taylor Bustamante <taylorbustamante16@gmail.com>
2021-05-27 15:55:17 -0700

    adding a controlled form

--- src/app/cars/add/add.component.ts
+++ src/app/cars/add/add.component.ts
@@ -7,19 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddComponent implements OnInit {
   status: string = 'status of the mouse';
+  name: string;
+  model: string;
+  color: string;
+
+  onAddName(event: Event) {
+    this.name = (<HTMLInputElement>event.target).value;
+  }
+
+  onAddModel(event: Event) {
+    this.model = (<HTMLInputElement>event.target).value;
+  }
+
+  onAddColor(event: Event) {
+    this.color = (<HTMLInputElement>event.target).value;
+  }
+
+  onSubmitCar() {
+    console.log(this.name);
+    console.log(this.model);
+    console.log(this.color);
+  }
   constructor() {}
 
-  onMouseEnter() {
-    this.status = 'Mouse has entered the div';
-  }
-  onMouseLeave() {
-    this.status = 'Mouse has left the div';
-  }
+  // onMouseEnter() {
+  //   this.status = 'Mouse has entered the div';
+  // }
+  // onMouseLeave() {
+  //   this.status = 'Mouse has left the div';
+  // }
 
-  alertOnClick(event: MouseEvent) {
-    console.log(event);
-    alert(`I was clicked on x: ${event.clientX} and y: ${event.clientY}`);
-  }
+  // alertOnClick(event: MouseEvent) {
+  //   console.log(event);
+  //   alert(`I was clicked on x: ${event.clientX} and y: ${event.clientY}`);
+  // }
 
   ngOnInit(): void {}
 }
